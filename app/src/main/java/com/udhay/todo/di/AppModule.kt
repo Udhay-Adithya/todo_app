6d7f18cd42332a9757f7744484714a431efcfa21
Udhay-Adithya <udhayxd@gmail.com>
2025-02-23 15:30:57 +0530

    feat: add di

---
+++ app/src/main/java/com/udhay/todo/di/AppModule.kt
@@ -0,0 +1,31 @@
+package com.udhay.todo.di
+
+import android.content.Context
+import androidx.room.Room
+import com.udhay.todo.data.local.AppDatabase
+import com.udhay.todo.data.local.dao.TodoDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideAppDatabase(@ApplicationContext context: Context) : AppDatabase{
+        return Room.databaseBuilder(
+            context,
+            AppDatabase::class.java,
+            "todo_db"
+        ).build()
+    }
+
+    fun provideTodoDao(database: AppDatabase) : TodoDao{
+        return database.getTodoDao()
+    }
+}
